a09ff508b700a18aeda4e20aeb00841aba2247a2
Mickael Stanislas <contact@mickael-stanislas.com>
2026-07-24 11:56:13 +0200

    chore(website): add .gitignore to exclude build artifacts and cache

--- website/.vitepress/dist/assets/chunks/swimlanesDiagram-G3AALYLV.CpMMtZ3c.js
+++
@@ -1,8 +0,0 @@
-import{c as r,s as e}from"./flowDiagram-23GEKE2U.DHep36pq.js";import{_ as a}from"./mermaid.core.DGaT5WHW.js";import"./chunk-5VM5RSS4.BL5aA_7U.js";import"./chunk-XXDRQBXY.BxP05VQP.js";import"./chunk-VR4S4FIN.BHw0qMkj.js";import"./chunk-32BRIVSS.DNfz5m88.js";import"./channel.runty9IN.js";import"./framework.QGT6ovkH.js";var o=a(t=>`${e(t)}
-  .swimlane.cluster rect {
-    stroke: ${t.clusterBorder} !important;
-  }
-  [data-look="neo"].cluster rect {
-    filter: none;
-  }
-`,"getStyles"),s=o,n=r({defaultLayout:"swimlane",styles:s});export{n as diagram};
